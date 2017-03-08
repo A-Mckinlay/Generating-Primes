721c5d1cd88af8661507a4bdd100947a454f53d0
A-Mckinlay <a.mckinlay@dundee.ac.uk>
2017-03-08 04:52:57 +0000

    Implemented basic sieve to upper bound given by user.

--- jasmine-standalone-2.5.2/src/generatePrime.js
+++ jasmine-standalone-2.5.2/src/generatePrime.js
@@ -1,11 +1,18 @@
 function main(){
-  console.log("hello");
   var start = new Date().getTime();
 
   var targetNumPrimes = getNumPrimes();
-  console.log(targetNumPrimes);
+  const lowerBound = 2;
+  var upperBound = targetNumPrimes;//Temporary, so I can adjust the upperBound easily.
+  var primes = [];
+
+  primes = simpleSieve(genNumList(lowerBound, upperBound));
+
+
+
   var end = new Date().getTime();
   var time = end - start;
+  printResult(primes, time);
 }
 
 function getNumPrimes()
@@ -13,4 +20,43 @@ function getNumPrimes()
   var numPrimes = parseInt(document.getElementById('numPrimes').value);
   if(numPrimes < 1 || numPrimes == NaN) return false;
   return numPrimes;
+}
+
+function genNumList(lowerBound, upperBound){
+  var numList = [];
+  for(var i=lowerBound; i<upperBound+1; i++)
+  {
+    numList.push(i);
+  }
+  return numList;
+}
+
+function simpleSieve(numList)
+{
+  var upperBoundSqrt = Math.ceil(Math.sqrt(numList[numList.length-1]))
+  for(var i=0; i<upperBoundSqrt; i++)
+  {
+    if(numList[i] != 0)
+    {
+      var p = (((numList[i]) * (numList[i]))-2)
+      for(p; p<numList.length; p += numList[i])
+      {
+          numList[p] = 0;
+      }
+    }
+  }
+
+  var primes = [];
+  for(var i=0; i<numList.length; i++)
+  {
+    if(numList[i] != 0)
+    primes.push(numList[i]);
+  }
+  return primes;
+}
+
+function printResult(primes, time)
+{
+  document.getElementById("Primes").innerHTML = primes.join();
+  document.getElementById("time").innerHTML = "Time taken: " + time +"ms";
 }
