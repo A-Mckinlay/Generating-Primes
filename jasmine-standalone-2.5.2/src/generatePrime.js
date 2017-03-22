ede518427a6a7c4fe93cc02b9c608818dcbde0f2
A-Mckinlay <a.mckinlay@dundee.ac.uk>
2017-03-22 16:10:45 +0000

    Removed 2-D array. Fixed timer.

--- jasmine-standalone-2.5.2/src/generatePrime.js
+++ jasmine-standalone-2.5.2/src/generatePrime.js
@@ -13,7 +13,7 @@ function main(){
   const lowerBound = 2;
   var upperBound = 100;
   var primes = [];
-
+  start = new Date().getTime();
   primes = simpleSieve(genNumList(lowerBound, upperBound));
   primesCheckedUpTo = upperBound;
 
@@ -39,7 +39,6 @@ function main(){
 function getNumPrimes(){
   var numPrimes = parseInt(document.getElementById('numPrimes').value);
   if(numPrimes < 1 || numPrimes == NaN){return false;}
-  start = new Date().getTime();
   return numPrimes;
 }
 
@@ -109,49 +108,39 @@ function printResult(primes, numPrimes)
   {
     display.push(primes[i]);
   }
+  primes.length = 0; //Delete the primes array to free up space
 
-  //Create 2-D array of multiplications:
-  var multiArray = [];
+  //Create multiplication table:
+  var htmlBuffer = "<table border='1'><tr>";
   for(var row=0; row<display.length+1; row++)
   {
-    multiArray[row] = [];
+    if(row>0) htmlBuffer += "<tr>";
     for(var cell=0; cell<display.length+1; cell++)
     {
-      if (row == 0 && cell == 0) {//Top left cell
-        multiArray[row][cell] = "";
-      } else if(row == 0) {//Top row
-        multiArray[row][cell] = display[cell -1];
+      if(row == 0) {//Top row
+        if(cell == 0)//Top left empty cell
+        {
+          htmlBuffer += "<td></td>"
+        }
+        else{
+          htmlBuffer += "<td>"+ display[cell-1] + "</td>";
+        }
       }else if (row>0 && cell == 0) {//Left most cell of row
-        multiArray[row][cell] = display[row-1];
+        htmlBuffer += "<td>"+ display[row-1] + "</td>";
       }else {//Any normal product cell. Using the transpose here would half the number of calculations
-        multiArray[row][cell] = display[row-1]*display[cell-1];
-      }
-    }
-  }
-
-  //Create HTML table:
-  var htmlBuffer = "<table border='1'><tr>";
-  for(var row=0; row<multiArray.length; row++)
-  {
-    if(row>0) htmlBuffer += "<tr>";
-    for(var cell=0; cell<multiArray[row].length; cell++)
-    {
-      if(row==0){//The top row
-        htmlBuffer += "<td>"+ multiArray[row][cell] + "</td>";
-      }else if(cell==0){//The left most cell of the row
-        htmlBuffer += "<td>"+ multiArray[row][0] + "</td>"
-      }else{//Any normal product cell{
-        htmlBuffer += "<td>"+ multiArray[row][cell] + "</td>";
+        htmlBuffer += "<td>"+ display[row-1]*display[cell-1] + "</td>";
       }
     }
     htmlBuffer += "</tr>";
   }
   htmlBuffer +="</table>";
 
-  document.getElementById("table").innerHTML = htmlBuffer;
 
-  //document.getElementById("Primes").innerHTML = display.join();
-  var end = new Date().getTime();
-  var time = end - start;
-  document.getElementById("time").innerHTML = "Time taken: " + time +"ms";
+  document.getElementById("table").innerHTML = htmlBuffer;
+  setTimeout(function () {
+    var end = new Date().getTime();
+    var totalTime = end - start;
+    document.getElementById("totalTime").innerHTML = "Time taken to generate primes and render table on screen: " + totalTime +"ms";
+  },10)
+
 }
